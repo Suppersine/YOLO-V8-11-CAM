9c005651bc64e4bb97ab8e3d827d2b9a41743eb8
Supasin Wuthikulphakdi <49264553+Suppersine@users.noreply.github.com>
2024-11-25 19:23:46 +0700

    includes GradCAM

--- yolo_cam/eigen_cam.py
+++ yolo_cam/eigen_cam.py
@@ -1,3 +1,4 @@
+import torch
 from yolo_cam.base_cam import BaseCAM
 from yolo_cam.utils.svd_on_activations import get_2d_projection
 
@@ -22,3 +23,22 @@ class EigenCAM(BaseCAM):
                       grads,
                       eigen_smooth):
         return get_2d_projection(activations)
+
+class GradCAM(BaseCAM):
+    def __init__(self, model, target_layers, task: str = 'od', #use_cuda=False,
+                 reshape_transform=None):
+        super(GradCAM, self).__init__(model,
+                                    target_layers,
+                                    task,
+                                    reshape_transform,
+                                    #use_cuda,
+                                    uses_gradients=True  # Grad-CAM requires gradients
+        )
+
+    def get_cam_weights(self, input_tensor, target_layers, targets, activations, grads):
+        """
+        Compute weights for Grad-CAM:
+        - Averaging gradients across the spatial dimensions.
+        """
+        # grads: Gradients of the activations w.r.t. the target
+        return torch.mean(grads, dim=(2, 3))  # BxC: Batch and Channels
